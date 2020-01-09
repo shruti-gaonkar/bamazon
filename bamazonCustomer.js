ed85b792ba256cb040074161c579b463b1392c12
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-01-09 10:30:07 -0800

    Added bamazon functionality to display products and ask customer to order product and update quantity and show price accordingly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,10 +18,11 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    queryAllItems();
+    getAllItems();
 });
 
-function queryAllItems() {
+
+function getAllItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log("Item Id | Product Name | Price");
@@ -28,5 +30,77 @@ function queryAllItems() {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
         }
         console.log("-----------------------------------");
+
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "item_id",
+                message: "Enter the ID of the product you would like to buy?"
+                /*validate: function validateInput(name) {
+                    if (!name.match(/^[a-zA-Z]+$/)) {
+                        return 'Only alphabets are allowed';
+                    }
+                    return name !== '';
+                }*/
+            },
+            {
+                type: "number",
+                name: "quantity",
+                message: "How many units of the product you would like to buy?"
+            }
+        ]).then(function (answers) {
+            getItem(answers)
+        });
     });
+}
+
+function getItem(answers) {
+    console.log("Selecting a product...\n");
+    var query = connection.query(
+        "SELECT * FROM products WHERE ?",
+        {
+            item_id: answers.item_id
+        }, function (err, res) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            if (res[0].quantity > 0) {
+                if (res[0].quantity >= answers.quantity) {
+                    updateProduct(res, answers.quantity);
+                } else {
+                    console.log(`Only ${res[0].quantity} left!`);
+                }
+            } else {
+                console.log("Insufficient quantity!");
+            }
+
+        });
+    console.log(query.sql);
+}
+
+function updateProduct(oldData, newQuantity) {
+    console.log("Updating all product quantities...\n");
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                quantity: (oldData[0].quantity - newQuantity)
+            },
+            {
+                item_id: oldData[0].item_id
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            //console.log(res);
+
+            let total_cost = oldData[0].price * newQuantity;
+            console.log(`The total cost of your purchase is $${total_cost}`);
+            // Call deleteProduct AFTER the UPDATE completes
+            // deleteProduct();
+        }
+    );
+
+    // logs the actual query being run
+    console.log(query.sql);
+    connection.end();
 }
