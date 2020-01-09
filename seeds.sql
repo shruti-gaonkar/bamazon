0ab81042cf388f39edc751c452f65a3ea7ad7f63
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-01-08 22:24:24 -0800

    Connecting to database and reading records

--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,5 @@
 USE bamazon;
 
 INSERT INTO products(product_name, department_name, price, quantity)
-VALUES ("shampoo", "beauty", 10.45, 3)
+VALUES ("shampoo", "beauty", 10.45, 3),
 ("soap", "beauty", 2.40, 5);
